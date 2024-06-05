ea063cf3cc2c7eb135b3542bc9703233d3e25b54
gathacom <gathacom2222@gmail.com>
2024-06-05 10:27:44 +0700

    add expenses to trips return

--- src/index.js
+++ src/index.js
@@ -11,12 +11,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(cors(
-    {
-        origin: ["*", "http://localhost:3000", "https://travel-expense-client-dot-project-akhir-tcc.as.r.appspot.com/" ],
-    }
-));
-
+app.use(
+  cors({
+    origin: [
+      "*",
+      "http://localhost:3000",
+      "https://travel-expense-client-dot-project-akhir-tcc.as.r.appspot.com"
+    ],
+  })
+);
 
 //midleware
 app.use(bodyParser.json());
@@ -25,21 +28,24 @@ app.use(express.json());
 // app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/users", async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.status(200).json({ users });
-})
+  const users = await prisma.user.findMany();
+  res.status(200).json({ users });
+});
 app.get("/", async (req, res) => {
-    res.status(200).send("Hello World");
-})
+  res.status(200).send("Hello World");
+});
 
 app.use("/", require("./routes/auth"));
 app.use("/", require("./routes/trip"));
 app.use("/", require("./routes/expense"));
 
-app.listen(PORT, () => console.log(`This Server started on port : ${PORT}` ));
+app.listen(PORT, () => console.log(`This Server started on port : ${PORT}`));
